5bee13083d4cdb1417ad5f053cd010e58734707c
Thimoty Wolf <83303337+ThimotyWolf@users.noreply.github.com>
2021-12-26 22:46:54 +0100

    PositionVonAutraegenGefixed

--- skripte/Wohngebäudepop.gd
+++ skripte/Wohngebäudepop.gd
@@ -8,19 +8,22 @@ extends PopupMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var wohn = get_parent()
+#	var wohn = get_parent()
 	#var wohnanchor = wohn.
-	var wohnpos = wohn.get_position()
+#	var wohnpos = wohn.get_position()
 	#print(wohnanchor)
-	print(wohnpos)
+#	print(wohnpos)
+	var pos = get_position()
+	var rel = self.get_canvas_transform()
+	print(rel)
+	print(pos)
 	#self.anchor_left = 100
 	#self.anchor_right = 100
 	#self.anchor_top = 100
 	#self.anchor_bottom = 100
-#	self.rect_position = Vector2(200,200)
 	add_item("drgw4g", 1, 0)
 	add_item("fxghrh", 2, 0)
-
+	self.rect_position = Vector2(1920/2-50,1080/2-50)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
