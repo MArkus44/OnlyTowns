56697df8f2259ba76472367234f9440710c9fa5d
Thimoty Wolf <83303337+ThimotyWolf@users.noreply.github.com>
2021-12-28 17:51:11 +0100

    wegWiederWasUnoetiges

--- skripte/Geld.gd
+++ skripte/Geld.gd
@@ -6,27 +6,6 @@ func _ready():
 # warning-ignore:unused_argument
 func _process(delta):
 	var geld_temp = str(get_geld())
-	var laenge = geld_temp.length()-1
-	var array_geld_temp = []
-# warning-ignore:unused_variable
-	var runde = 1
-# warning-ignore:unused_variable
-	var runde_mal = int(laenge / 3)
-	for c in geld_temp:
-		array_geld_temp.append(c)
-	
-#	for i in range(runde_mal):
-#		runde = runde + 1
-#		var array_geld_temp2 = [laenge+1]
-#		for k in range(laenge-3):
-#			array_geld_temp2[k] = array_geld_temp[k]
-#		array_geld_temp2[laenge - 3*runde + 3] = array_geld_temp[laenge - 3*runde + 2]
-#		array_geld_temp2[laenge - 3*runde + 2] = array_geld_temp[laenge - 3*runde + 1]
-#		array_geld_temp2[laenge - 3*runde + 1] = array_geld_temp[laenge - 3*runde + 0]
-#		array_geld_temp2[laenge - 3*runde] = "."
-#		laenge + 1
-#		array_geld_temp = array_geld_temp2
-	print(array_geld_temp)
 	self.text = geld_temp
 	
 func get_geld():
