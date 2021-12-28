cbd61d157ef44533bbf4e1b59b8bcd5d1d30c6f7
MArkus44 <82948725+MArkus44@users.noreply.github.com>
2021-12-28 22:11:52 +0100

    weiter tausenderpunkte und fonts angepasst

--- skripte/Conf.gd
+++ skripte/Conf.gd
@@ -12,6 +12,18 @@ func _ready():
 
 func _on_PopupMenu_id_pressed(id):
 	var gebaude = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().antrag_gebaeude(id)
-	var kosten = str((get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().gebaeude_array[id].get_kosten())*-1)
+	var kosten =comma_sep(str((get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().gebaeude_array[id].get_kosten())*-1))
 	dialog_text = "Möchten sie " + gebaude + " für " + kosten + " € wirklich bauen?" 
 	popup()
+
+func comma_sep(number):
+	var string = str(number)
+	var mod = string.length() % 3
+	var res = ""
+
+	for i in range(0, string.length()):
+		if i != 0 && i % 3 == mod:
+			res += ","
+		res += string[i]
+
+	return res
