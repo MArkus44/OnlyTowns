0edef84c176db2a5e0440a62ff9948e9fc2d58b1
Thimoty Wolf <83303337+ThimotyWolf@users.noreply.github.com>
2022-01-03 22:35:09 +0100

    LabelsFirmenSolltenGehen

--- skripte/FirmaWindow.gd
+++ skripte/FirmaWindow.gd
@@ -9,9 +9,17 @@ extends WindowDialog
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_name_bauunternehmen()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var n1 = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().randomG()
+	var n2 = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().randomG()
+	while(n2 == n1):
+		n2 = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().randomG()
+	var n3 = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().randomG()
+	while(n3 == n2 || n3 == n1):
+		n3 = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().randomG()
+	var n4 = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().randomG()
+	while(n4 == n3 || n4 == n2 || n4 == n1):
+		n4 = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().randomG()
+	$Firma1.text = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_name_bauunternehmen(n1)
+	$Firma2.text = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_name_bauunternehmen(n2)
+	$Firma3.text = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_name_bauunternehmen(n3)
+	$Firma4.text = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_name_bauunternehmen(n4)
